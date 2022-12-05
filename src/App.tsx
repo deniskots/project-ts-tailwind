b86980b28740588405b80d24e19b1dfb41bb04d0
denisko <denis374@gmail.com>
2022-12-05 15:50:36 +0200

    first render show your articles if you login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {Header} from "./components/header/header.comp";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useMatch, useNavigate} from "react-router-dom";
 import {routes} from "./utils/routes";
 
 interface AppProps {}
 
 export const App: FC<AppProps> = ({}) => {
+    const navigate = useNavigate()
+
+    const homePageArticles = useMatch(routes.globalArticles.path)
+
+    useEffect(() => {
+        if(homePageArticles) {
+            navigate(routes.personalArticles.path)
+        }
+    },[])
   return (
     <div className='pb-16 rounded-3xl'>
         <Header/>
